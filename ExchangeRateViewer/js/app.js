859a19e5026e79945fa76281d88725e7df995d95
pszczolkowski <pszczolkowski.k@gmail.com>
2016-06-22 06:57:32 +0200

    Create main page

---
+++ ExchangeRateViewer/js/app.js
@@ -0,0 +1,60 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('exchangeRateViewerApp', [
+            'chart.js',
+            'ngRoute'
+        ])
+
+        .config(['$locationProvider', '$routeProvider', function config($locationProvider, $routeProvider) {
+               $routeProvider
+                  .when('/', {
+                      templateUrl: 'main.html',
+                      controller: 'mainController'
+                  })
+                  .when('/details/:currencyCode', {
+                      templateUrl: 'details.html',
+                      controller: 'detailsController'
+                  })
+                  .otherwise('/');
+            }
+        ])
+        
+        .run(['$rootScope', '$location', 'stateKeeper', function ($rootScope, $location, stateKeeper) {
+            var app = WinJS.Application;
+            var activation = Windows.ApplicationModel.Activation;
+
+            app.onactivated = function (args) {
+                if (args.detail.kind === activation.ActivationKind.launch) {
+                    if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                        // TODO: This application has been newly launched. Initialize your application here.
+                    } else {
+                        // TODO: This application was suspended and then terminated.
+                        // To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+                        console.log('Restoring state');
+                        var state = stateKeeper.getState();
+                        
+                        if (state['$path']) {
+                            $location.path(state['$path']);
+                        }
+                    }
+                    args.setPromise(WinJS.UI.processAll());
+                }
+            };
+
+            app.oncheckpoint = function (args) {
+                // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+                // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+                // If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+                stateKeeper.saveState();
+            };
+
+            app.start();
+            WinJS.UI.processAll();
+
+            $rootScope.$on('$routeChangeStart', function (next, current) {
+                stateKeeper.saveState();
+            });
+        }]);
+})();
---
+++ ExchangeRateViewer/js/controller/mainController.js
@@ -0,0 +1,92 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('exchangeRateViewerApp')
+        .controller('mainController', mainController);
+
+
+    mainController.$inject = ['$scope', '$location', '$timeout', 'currencyLoader', 'dateUtils', 'stateKeeper'];
+
+    function mainController($scope, $location, $timeout, currencyLoader, dateUtils, stateKeeper) {
+        $scope.currencies = [];
+        $scope.showCurrencyDetails = showCurrencyDetails;
+        $scope.loadCurrencies = loadCurrencies;
+        $scope.close = close;
+        $scope.selectToday = selectToday;
+
+        $scope.date = restoreDate() || dateUtils.today();
+        loadCurrencies();
+
+        var datePicker = $('#datepickerButton')
+            .fdatepicker({
+                initialDate: dateUtils.dateToString($scope.date),
+                format: 'dd-mm-yyyy',
+                onRender: function (date) {
+                    if (date.getFullYear() < 2002) {
+                        return 'disabled';
+                    } else {
+                        return date.valueOf() > (new Date()).valueOf() ? 'disabled' : '';
+                    }
+	            }
+            })
+            .on('changeDate', e => {
+                $scope.date = dateUtils.toUtc1(e.date);
+                loadCurrencies();
+            }).data('datepicker');;
+
+        stateKeeper.addSuspensionListener(onSuspend);
+
+
+        function loadCurrencies() {
+            $scope.loading = true;
+            $scope.noInternetConnection = false;
+            $scope.error = false;
+
+            currencyLoader
+                .loadCurrenciesForDate($scope.date)
+                .then(loadedCurrencies => {
+                    $scope.currencies = loadedCurrencies;
+                    $timeout(() => $scope.$digest());
+                }, err => {
+                    $scope.loading = false;
+
+                    if (err === 'NO_INTERNET_CONNECTION') {
+                        $scope.noInternetConnection = true;
+                        $timeout(angular.noop);
+                    } else {
+                        $scope.error = true;
+                        console.log('ERROR: ' + err);
+                        $timeout(angular.noop);
+                    }
+                }).done(x => {
+                    $scope.loading = false;
+                });
+        }
+
+        function showCurrencyDetails(currency) {
+            $location.path('/details/' + currency.code);
+        }
+
+        function onSuspend(state) {
+            state['main'] = {
+                date: $scope.date.getTime()
+            };
+        }
+
+        function restoreDate() {
+            return stateKeeper.getState()['main'] && new Date(stateKeeper.getState()['main'].date);
+        }
+
+        function close() {
+            window.close();
+        }
+
+        function selectToday() {
+            $scope.date = dateUtils.today();
+            datePicker.update($scope.date);
+            loadCurrencies();
+        }
+        
+    }
+})();
