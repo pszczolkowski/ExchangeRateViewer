d02263dd4c8fea3d0bdb2982b9fbfb7e88de3ca4
pszczolkowski <pszczolkowski.k@gmail.com>
2016-06-22 06:58:11 +0200

    Create state keeping service

---
+++ ExchangeRateViewer/js/service/stateKeeper.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('exchangeRateViewerApp')
+        .factory('stateKeeper', stateKeeper);
+
+
+    stateKeeper.$inject = ['$location'];
+
+    function stateKeeper($location) {
+        var listeners = [];
+
+        return {
+            addSuspensionListener: addSuspensionListener,
+            saveState: saveState,
+            getState: getState
+        };
+
+
+        function addSuspensionListener(listener) {
+            listeners.push(listener);
+        }
+
+        function saveState() {
+            var state = WinJS.Application.sessionState;
+            listeners.forEach(listener => listener(state));
+
+            state['$path'] = $location.path();
+        }
+
+        function getState() {
+            return WinJS.Application.sessionState;
+        }
+    }
+})();
